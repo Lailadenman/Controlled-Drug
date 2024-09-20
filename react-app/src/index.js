fbaf0d653753e54e4c2db10073512471fff91dff
Laila <lailadenman@gmail.com>
2024-09-20 09:13:24 -0700

    updates

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App'
 
-ReactDOM.render(
+const root = createRoot(App)
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
